6b26ca410a38b1cc377370e4525baf21f11e229a
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-05-07 17:22:26 -0300

    fix: broken storybook build

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,21 +1,18 @@
-module.exports = {
-  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
-  "framework": {
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/react-vite"
+  ],
+  framework: {
     name: "@storybook/react-vite",
-    options: {}
-  },
-  "features": {
-    "storyStoreV7": true
+    options: {},
   },
   docs: {
-    autodocs: true
+    autodocs: "tag",
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/';
-    }
-
-    return config
-  }
 };
+export default config;
